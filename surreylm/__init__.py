50e6a2b4586c7eaf25649b92a60cec63e4959a19
Holly Sherlock <holly.sherlock@surrey.ac.uk>
2023-08-10 15:08:37 +0100

    Initial commit

---
+++ surreylm/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "lmdatabase.db"
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'gdwjama bbawdjkwjdw'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    from .auth import auth
+
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .database_models import User, Academic, Vendor, Lab, Software, SoftwareLab
+
+    #with app.app_context():
+    #    db.create_all()
+
+      #  default_user = User(email='example@cat.com', first_name='Default', last_name='Admin', password='password',
+       #                     admin=True)
+        #db.session.add(default_user)
+        #db.session.commit()
+
+    return app
---
+++ surreylm/database_models.py
@@ -0,0 +1,53 @@
+from . import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.Text, unique=True)
+    first_name = db.Column(db.Text)
+    last_name = db.Column(db.Text)
+    password = db.Column(db.Text)
+    admin = db.Column(db.Boolean, default=False)
+
+
+class Academic(db.Model):
+    __tablename__ = 'academics'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.Text, unique=True)
+    first_name = db.Column(db.Text)
+    last_name = db.Column(db.Text)
+    password = db.Column(db.Text)
+    phone_extension = db.Column(db.Integer)
+
+
+class Vendor(db.Model):
+    __tablename__ = 'vendors'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    phone = db.Column(db.Text)
+    email = db.Column(db.Text)
+
+
+class Lab(db.Model):
+    __tablename__ = 'labs'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    room = db.Column(db.Text)
+
+
+class Software(db.Model):
+    __tablename__ = 'software'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    version = db.Column(db.Text)
+    license_expiry = db.Column(db.DateTime)
+    vendor_id = db.Column(db.Integer, db.ForeignKey('vendors.id'))
+    academic_id = db.Column(db.Integer, db.ForeignKey('academics.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+
+class SoftwareLab(db.Model):
+    __tablename__ = 'software_labs'
+    id = db.Column(db.Integer, primary_key=True)
+    software_id = db.Column(db.Integer, db.ForeignKey('software.id'))
+    lab_id = db.Column(db.Integer, db.ForeignKey('labs.id'))
