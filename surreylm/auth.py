97bb951b009ceb2ce42304e6f4e630f2ff7ea648
Holly Sherlock <holly.sherlock@surrey.ac.uk>
2023-08-17 22:27:56 +0100

    progress on login and sign up

--- surreylm/auth.py
+++ surreylm/auth.py
@@ -26,6 +26,38 @@ def login():
 
     return render_template("login.html", user=current_user)
 
+
+@auth.route('/register', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            new_user = User(email=email, first_name=first_name, last_name=last_name,
+                            password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
+    return render_template("register.html", user=current_user)
+
+
 @auth.route('/logout')
 def logout():
-    return "<h1>Logged out</h1>"
+    return redirect(url_for('auth.login'))
