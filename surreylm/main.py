c5e3cb6d96066d89c01a2320e512b691e7982052
Holly Sherlock <holly.sherlock@surrey.ac.uk>
2024-02-10 15:03:28 +0000

    logging

--- surreylm/main.py
+++ surreylm/main.py
@@ -6,18 +6,9 @@
 # Importing the required modules.
 from surreylm import create_app
 
-# from logging import FileHandler, INFO
 
 app = create_app()  # Creating the flask app.
 
-# Creating the error log.
-# log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-# file_handler = FileHandler('errorlog.txt')
-# file_handler.setLevel(INFO)
-# formatter = logging.Formatter(log_format)
-# file_handler.setFormatter(formatter)
-# app.logger.addHandler(file_handler)
-# app.logger.setLevel(INFO)
 
 if __name__ == '__main__':
     app.run()  # Running the application.
